c76920a0ec8be04135b27e7eef3cf06d987a50d9
erikn69 <erikn_69@hotmail.com>
2025-03-11 10:12:08 -0500

    Try2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
         './node_modules/@flareapp/ignition-ui/src/**/*.{js,jsx,ts,tsx}',
     ],
     darkMode: 'class', // or 'media' or 'class'
-    // important: true,
+    important: '#app',
     theme: {
         extend: {
             colors: {
